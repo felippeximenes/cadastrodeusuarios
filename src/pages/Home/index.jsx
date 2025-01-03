0c9baff9a7853b3bd60931a2e25a706b64e6d800
felippeximenes <felippelpximenes@outlook.com>
2025-01-03 15:09:46 -0300

    mapeando os usuarios

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,53 @@
 
 import './style.css'
+import Trash from '../../assets/lixo.svg'
 
 function Home() {
-  
+
+  const users = [{
+    id: '234sfafsasf',
+    name: 'Rodolfo',
+    age: 33,
+    email: 'felippe@emmail.com',
+  }, {
+    id: '1231afafg',
+    name: 'Eliana',
+    age: 38,
+    email: 'eliana@emmail.com',
+  }, {
+    id: '1231afafg',
+    name: 'Fulana',
+    age: 22,
+    email: 'fulana@emmail.com',
+  }]
+
 
   return (
     <div className='container'>
       <form action="">
         <h1>Cadastro de Usúarios</h1>
-        <input name="nome" type='text'/>
-        <input name="idade" type='number'/>
+        <input name="nome" type='text' />
+        <input name="idade" type='number' />
         <input name="email" type='email' />
         <button type='button'>Cadastrar</button>
       </form>
+
+      {users.map(user => (
+        <div key={user.id} >
+          <div>
+            <p>Nome:{user.name}</p>
+            <p>Idade:{user.age}</p>
+            <p>Email: {user.email}</p>
+          </div>
+          <button>
+            <img src={Trash} />
+          </button>
+        </div>
+      ))}
+
+    
+
+      
     </div>
 
   )
